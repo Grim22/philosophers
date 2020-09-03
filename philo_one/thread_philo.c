51e5689bcf159703794fa165816c9a6de7b9fb76
Basile <basile.brunet@gmail.com>
2020-09-03 11:45:03 +0200

    subfunctions create_death_thread & check_eat_number ; new prototype for ft_print_status

--- philo_one/thread_philo.c
+++ philo_one/thread_philo.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   m_cycle.c                                          :+:      :+:    :+:   */
+/*   thread_philo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbrunet <bbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/31 18:19:19 by bbrunet           #+#    #+#             */
-/*   Updated: 2020/09/02 18:13:33 by bbrunet          ###   ########.fr       */
+/*   Updated: 2020/09/03 11:42:55 by bbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_print_status_end(int status)
 		ft_putendl_fd(" died", 1);
 }
 
-void	ft_print_status(int status, int id, pthread_mutex_t *display, long int timestamp_start)
+void	ft_print_status(int status, t_options *options)//int id, pthread_mutex_t *display, long int timestamp_start)
 {
 	struct timeval current_t;
 	char *timestamp;
@@ -34,19 +34,19 @@ void	ft_print_status(int status, int id, pthread_mutex_t *display, long int time
 	long int elapsed;
 	long int current_time;
 	
-	pthread_mutex_lock(display); // partie locked pour que les threads n'affichent pas les statuts en meme temps
+	pthread_mutex_lock(options->display); // partie locked pour que les threads n'affichent pas les statuts en meme temps
 	gettimeofday(&current_t, NULL);
 	current_time = current_t.tv_sec * 1000 + current_t.tv_usec / 1000;
-	elapsed = current_time - timestamp_start;
+	elapsed = current_time - options->timestamp_start;
 	timestamp = ft_itoa(elapsed);
 	ft_putstr_fd(timestamp, 1);
 	free(timestamp);
 	ft_putstr_fd(" ", 1);
-	identifier = ft_itoa((long)id);
+	identifier = ft_itoa((long)options->identifier);
 	ft_putstr_fd(identifier, 1);
 	free(identifier);
 	ft_print_status_end(status);
-	pthread_mutex_unlock(display);
+	pthread_mutex_unlock(options->display);
 }
 
 void	unlock_forks(t_options *options)
@@ -69,7 +69,7 @@ void	lock_forks(t_options *options)
 			if (pthread_mutex_lock(options->fork_r))
 				printf("error lock\n");
 		}
-		ft_print_status(FORK, options->identifier, options->display, options->timestamp_start);
+		ft_print_status(FORK, options);
 		if (options->identifier % 2 == 0)
 		{
 			if (pthread_mutex_lock(options->fork_r))
@@ -80,7 +80,7 @@ void	lock_forks(t_options *options)
 			if (pthread_mutex_lock(options->fork_l))
 				printf("error lock\n");
 		}
-		ft_print_status(FORK, options->identifier, options->display, options->timestamp_start);
+		ft_print_status(FORK, options);
 }
 
 void	*death_alarm(void *void_options)
@@ -106,7 +106,7 @@ void	*death_alarm(void *void_options)
 		if (elapsed_time > max)
 		{
 			options->died = YES;
-			ft_print_status(DIE, options->identifier, options->display, options->timestamp_start);
+			ft_print_status(DIE, options);
 			return (NULL);
 		}
 		usleep(1000); // 1 ms (pas plus de 10ms entre la mort et l'affichage de la mort)
@@ -120,34 +120,48 @@ void	set_latest_meal_time(t_options *options)
 	gettimeofday(&current_t, NULL);
 	options->latest_meal = current_t.tv_sec * 1000 + current_t.tv_usec / 1000;
 }
+int		create_death_thread(t_options *options)
+{
+	pthread_t death_thread;
+
+	if (pthread_create(&death_thread, NULL, &death_alarm, options))
+	{
+		ft_putendl_fd("pthread_create failed", 2);
+		return (EXIT_FAILURE);
+	}
+	pthread_detach(death_thread);
+	return (EXIT_SUCCESS);
+}
+
+void	check_eat_number(int *counter, t_options *options)
+{
+	if (options->num_of_time == UNSET)
+		return ;
+	(*counter)++;
+	if (*counter >= options->num_of_time)
+		options->enough_food = YES;
+}
 
 void	*cycle(void *void_options)
 {
 	t_options	*options;
-	pthread_t	clock;
 	int			count_eat;
 	
 	options = (t_options*)void_options;
 	count_eat = 0;
-	
-	if (pthread_create(&clock, NULL, &death_alarm, options))
-		printf("problem\n");
-	pthread_detach(clock);
-	
+	if (create_death_thread(options) == EXIT_FAILURE)
+		return (NULL);
 	while (1)
 	{
 		lock_forks(options);
 		set_latest_meal_time(options);
-		ft_print_status(EAT, options->identifier, options->display, options->timestamp_start);
-		count_eat++;
-		if (options->num_of_time != UNSET && count_eat >= options->num_of_time)
-			options->enough_food = YES;
+		ft_print_status(EAT, options);
+		check_eat_number(&count_eat, options);
 		usleep(options->t_to_eat * 1000);	
 		unlock_forks(options);
-		
-		ft_print_status(SLEEP, options->identifier, options->display, options->timestamp_start);
+		ft_print_status(SLEEP, options);
 		usleep(options->t_to_sleep * 1000);	
-		ft_print_status(THINK, options->identifier, options->display, options->timestamp_start);
+		ft_print_status(THINK, options);
 	}
 	return (NULL);
 }
