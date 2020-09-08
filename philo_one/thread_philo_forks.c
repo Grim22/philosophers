bc902503acfa96baed979811af5378b357ffdc5d
Basile <basile.brunet@gmail.com>
2020-09-08 17:58:04 +0200

    no more differentiation btw even and odd in lock_forks

--- philo_one/thread_philo_forks.c
+++ philo_one/thread_philo_forks.c
@@ -6,7 +6,7 @@
 /*   By: bbrunet <bbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/03 18:04:34 by bbrunet           #+#    #+#             */
-/*   Updated: 2020/09/03 18:04:44 by bbrunet          ###   ########.fr       */
+/*   Updated: 2020/09/08 17:57:27 by bbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,45 +27,45 @@ int		unlock_forks(t_options *options)
 	return (EXIT_SUCCESS);
 }
 
-int		lock_forks_even(t_options *options)
-{
-	if (pthread_mutex_lock(options->fork_l) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	ft_print_status(FORK, options);
-	if (pthread_mutex_lock(options->fork_r) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	ft_print_status(FORK, options);
-	return (EXIT_SUCCESS);
-}
-
-int		lock_forks_odd(t_options *options)
-{
-	if (pthread_mutex_lock(options->fork_r) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	ft_print_status(FORK, options);
-	if (pthread_mutex_lock(options->fork_l) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	ft_print_status(FORK, options);
-	return (EXIT_SUCCESS);
-}
-
 int		lock_forks(t_options *options)
 {
-	if (options->identifier % 2 == 0)
-	{
-		if (lock_forks_even(options) == EXIT_FAILURE)
-		{
-			ft_putendl_fd("lock fork failed", 2);
-			return (EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		if (lock_forks_odd(options) == EXIT_FAILURE)
-		{
-			ft_putendl_fd("lock fork failed", 2);
-			return (EXIT_FAILURE);
-		}
-	}
+	if (pthread_mutex_lock(options->fork_l) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	ft_print_status(FORK, options);
+	if (pthread_mutex_lock(options->fork_r) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	ft_print_status(FORK, options);
 	return (EXIT_SUCCESS);
 }
+
+// int		lock_forks_odd(t_options *options)
+// {
+// 	if (pthread_mutex_lock(options->fork_r) == EXIT_FAILURE)
+// 		return (EXIT_FAILURE);
+// 	ft_print_status(FORK, options);
+// 	if (pthread_mutex_lock(options->fork_l) == EXIT_FAILURE)
+// 		return (EXIT_FAILURE);
+// 	ft_print_status(FORK, options);
+// 	return (EXIT_SUCCESS);
+// }
+
+// int		lock_forks(t_options *options)
+// {
+// 	if (options->identifier % 2 == 0)
+// 	{
+// 		if (lock_forks_even(options) == EXIT_FAILURE)
+// 		{
+// 			ft_putendl_fd("lock fork failed", 2);
+// 			return (EXIT_FAILURE);
+// 		}
+// 	}
+// 	else
+// 	{
+// 		if (lock_forks_odd(options) == EXIT_FAILURE)
+// 		{
+// 			ft_putendl_fd("lock fork failed", 2);
+// 			return (EXIT_FAILURE);
+// 		}
+// 	}
+// 	return (EXIT_SUCCESS);
+// }
