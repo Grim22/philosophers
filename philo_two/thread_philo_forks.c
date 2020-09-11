e7cd349ed5cedd22b5baa8d32caf65c7ba131a19
Basile <basile.brunet@gmail.com>
2020-09-11 18:05:53 +0200

    back to philo_two: functions lock_forks and unlock_forks used

--- philo_two/thread_philo_forks.c
+++ philo_two/thread_philo_forks.c
@@ -6,40 +6,40 @@
 /*   By: bbrunet <bbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/03 18:04:34 by bbrunet           #+#    #+#             */
-/*   Updated: 2020/09/11 10:48:56 by bbrunet          ###   ########.fr       */
+/*   Updated: 2020/09/11 17:57:05 by bbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "one.h"
 
-// int	unlock_forks(t_options *options)
-// {
-// 	if (pthread_mutex_unlock(options->fork_l))
-// 	{
-// 		ft_putendl_fd("unlock fork failed", 2);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	if (pthread_mutex_unlock(options->fork_r))
-// 	{
-// 		ft_putendl_fd("unlock fork failed", 2);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
+int	unlock_forks(t_options *options)
+{
+	if (sem_post(options->sem))
+	{
+		ft_putendl_fd("unlock fork failed", 2);
+		return (EXIT_FAILURE);
+	}
+	if (sem_post(options->sem))
+	{
+		ft_putendl_fd("unlock fork failed", 2);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
 
-// int	lock_forks(t_options *options)
-// {
-// 	if (pthread_mutex_lock(options->fork_l) == EXIT_FAILURE)
-// 	{
-// 		ft_putendl_fd("lock fork failed", 2);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	ft_print_status(FORK, options);
-// 	if (pthread_mutex_lock(options->fork_r) == EXIT_FAILURE)
-// 	{
-// 		ft_putendl_fd("lock fork failed", 2);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	ft_print_status(FORK, options);
-// 	return (EXIT_SUCCESS);
-// }
+int	lock_forks(t_options *options)
+{
+	if (sem_wait(options->sem))
+	{
+		ft_putendl_fd("lock fork failed", 2);
+		return (EXIT_FAILURE);
+	}
+	ft_print_status(FORK, options);
+	if (sem_wait(options->sem))
+	{
+		ft_putendl_fd("lock fork failed", 2);
+		return (EXIT_FAILURE);
+	}
+	ft_print_status(FORK, options);
+	return (EXIT_SUCCESS);
+}
